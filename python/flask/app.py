41128cd615b2e15217bc32208c27e0bc6137fdb6
pabpereza <pabloperezaradros@gmail.com>
2024-01-24 17:55:17 +0100

    Rust multi-stage

--- python/flask/app.py
+++ python/flask/app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world'
+    return 'Hello Youtube'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
 
---
+++ python/simple/app.py
@@ -0,0 +1,7 @@
+import time
+counter = 0
+
+while True:
+    counter +=1
+    print("Hello World: " + str(counter))
+    time.sleep(2)
